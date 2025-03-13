2f79a1f82601a16ba75b0513ae96a074a7f36692
Roddri123456 <rodhdzg@gmail.com>
2025-03-13 14:47:59 -0600

    added a new file, with its required file
    this thingymabob "gradients but cooler" shows several graphs with several ponts, cool and all, it also needs the "housing.csv" to be in the same folder to and the same path to be able to be read by the thingymabob "gradients but cooler"

---
+++ gradientsbutcooler.py
@@ -0,0 +1,8 @@
+#Gradients but batch, estocastic and mini-batch
+import numpy as np
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+
+#load datase with pandas
+X =
